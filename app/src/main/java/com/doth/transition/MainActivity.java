6434b2a7b286c7180fc84d7270aa3b2d123986b5
hiroki.tanida <hiroki.tanida@leverages.jp>
2016-02-11 20:04:50 +0900

    fix

--- app/src/main/java/com/doth/transition/MainActivity.java
+++ app/src/main/java/com/doth/transition/MainActivity.java
@@ -1,13 +1,13 @@
 package com.doth.transition;
 
 import android.content.Intent;
-import android.media.session.PlaybackState;
+//import android.media.session.PlaybackState;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+//import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,13 +16,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 //import com.doth.transition;
 
-import android.app.Activity;
-import android.os.Bundle;
+//import android.app.Activity;
+//import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonSegue;
-    private Button toastButton;
+//    private Button buttonSegue;
+//    private Button toastButton;
 
     TextView tvOutput;
     Button btOpen;
@@ -52,27 +52,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        buttonSegue = (Button)findViewById(R.id.buttonSegue);
-        buttonSegue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Intent intent = new Intent();
-                intent.setClassName("com.doth.transition", "com.doth.transition.SubActivity");
-                // intentへ添え字付で値を保持させる
-                intent.putExtra( "name", "hogehoge" );
-                startActivityForResult(intent, 0);
-            }
-        });
-
-        toastButton = (Button)findViewById(R.id.toastButton);
-        toastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "aaaa", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        buttonSegue = (Button)findViewById(R.id.buttonSegue);
+//        buttonSegue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//                Intent intent = new Intent();
+//                intent.setClassName("com.doth.transition", "com.doth.transition.SubActivity");
+//                // intentへ添え字付で値を保持させる
+//                intent.putExtra( "name", "hogehoge" );
+//                startActivityForResult(intent, 0);
+//            }
+//        });
+//
+//        toastButton = (Button)findViewById(R.id.toastButton);
+//        toastButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(), "aaaa", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
